2829c9f676b39c4f9875480baf62664f4f9f06f8
Aditya Babu Prajapati <108863108+adityaprajapati10@users.noreply.github.com>
2024-07-09 18:41:16 +0530

    Time: 6 ms (11.02%), Space: 72.7 MB (58.37%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.java
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -1,9 +1,8 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        int order = customers[0][0];
-        int comp =  customers[0][0]+ customers[0][1];
-        double totalWait = comp - order;
-        for(int i = 1 ; i < customers.length ; i++){
+        int comp =  0;
+        double totalWait = 0;
+        for(int i = 0 ; i < customers.length ; i++){
             if(customers[i][0]  <= comp){
                 comp += customers[i][1];
             }
