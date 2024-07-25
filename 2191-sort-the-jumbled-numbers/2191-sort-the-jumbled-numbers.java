4f9f2dd03dd22bbfe1ddd503364183c41be809ee
Aditya Babu Prajapati <108863108+adityaprajapati10@users.noreply.github.com>
2024-07-25 11:29:27 +0530

    Time: 67 ms (95.5%), Space: 55.6 MB (46.85%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
@@ -11,15 +11,51 @@ class Solution {
     }
     public int[] sortJumbled(int[] mapping, int[] nums) {
         int n = nums.length;
-        int[][] mappedNums = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            mappedNums[i][0] = nums[i];
-            mappedNums[i][1] = getNumber(nums[i], mapping);
+        int[] temp = new int[nums.length];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = getNumber(nums[i], mapping);
         }
-        Arrays.sort(mappedNums, (a, b) -> Integer.compare(a[1], b[1]));
-        for (int i = 0; i < n; i++) {
-            nums[i] = mappedNums[i][0];
-        }
+        mergeSort(nums , temp , 0 , temp.length-1);
         return nums;
+    }
+    public static void mergeSort(int[] nums , int[] temp , int l , int h){
+        if(l < h){
+            int mid = l + (h-l)/2;
+            mergeSort(nums , temp , l ,mid);
+            mergeSort(nums , temp , mid+1 , h);
+            mergeProcedure(nums , temp , l , mid , h);
+        }
+    }
+    public static void mergeProcedure(int[] nums , int[] temp , int l , int mid , int h){
+        int[] lSubNums = Arrays.copyOfRange(nums , l , mid+1);
+        int[] rSubNums = Arrays.copyOfRange(nums , mid+1 , h+1);
+        int[] lSubTemp = Arrays.copyOfRange(temp , l , mid+1);
+        int[] rSubTemp = Arrays.copyOfRange(temp , mid+1 , h+1);
+        int i = 0 , j = 0 , k = l;
+        while(i < lSubTemp.length && j < rSubTemp.length){
+            if(lSubTemp[i] <= rSubTemp[j]){
+                temp[k] = lSubTemp[i];
+                nums[k] = lSubNums[i];
+                i++;
+            }
+            else{
+                temp[k] = rSubTemp[j];
+                nums[k] = rSubNums[j];
+                j++;
+            }
+            k++;
+        }
+        while(i < lSubTemp.length){
+            temp[k] = lSubTemp[i];
+            nums[k] = lSubNums[i];
+            i++;
+            k++;
+        }
+        while(j < rSubTemp.length){
+            temp[k] = rSubTemp[j];
+            nums[k] = rSubNums[j];
+            j++;
+            k++;
+        }
     }
 }
